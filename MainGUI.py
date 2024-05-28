5f6a965f3066ad2f10536b547ef8c151486d7cc6
Dos-kim <glxmtmzlf@gmail.com>
2024-05-28 17:24:57 +0900

    Initial commit

---
+++ MainGUI.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class MAINGUI:
+    def __init__(self):
+        self.window = Tk()
+        self.window.geometry('1200x900')
+
+        self.window.mainloop()
+
+
+MAINGUI()
